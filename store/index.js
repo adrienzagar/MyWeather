4a067b0773474bf67f98456d2e732a3ddc1e77b1
adrien zgr <adrien.zagar@gmail.com>
2020-09-04 22:28:09 +0200

    add redux dev tools

--- store/index.js
+++ store/index.js
@@ -1,9 +1,18 @@
-import { createStore } from 'redux';
+//! == Import : npm
+import { createStore, compose, applyMiddleware  } from 'redux';
 
+//! == Import : local
 import rootReducer from '../reducers/index'
+
+//! == Enhancers
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const enhancers = composeEnhancers()
 
 const store = createStore(
     rootReducer,
+      //? preloadedState,
+     enhancers,
 );
 
 export default store;
